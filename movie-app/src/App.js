576dc210c2da523e120b9cd2bf653cbdfd015741
AhnDogeon <qltiqlti@gmail.com>
2021-05-09 23:10:27 +0900

    210509 | make component React

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Potato from './potato';
 
+function Coding() {
+    return <h1>I LIke Coding</h1>;
+}
+
+
 function App() {
   return (
     <div className="App">
       <h1>Hello!</h1>
       <Potato />
+      <Coding />
     </div>
   );
 }
